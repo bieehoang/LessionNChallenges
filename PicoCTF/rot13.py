69ef57b019b0b723b82144d19382125e75d0e110
Qiese <hoangtrungbi2002@gmail.com>
2022-11-13 23:01:42 +0700

    PicoCTF - ROT13

---
+++ PicoCTF/rot13.py
@@ -0,0 +1,17 @@
+Head = "Cryptography can be easy, do you know what ROT13 is?"
+key = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_uJdSftmh}"
+
+#def roi13(key):
+#    ans = ""
+#    for i in key:
+#        if i.isdigit():
+#            continue
+#        elif i.isascii():
+#
+#    # print(ans)
+#roi13(key)
+
+import string 
+print(string.ascii_lowercase[:13])
+print(string.ascii_uppercase[13:])
+
