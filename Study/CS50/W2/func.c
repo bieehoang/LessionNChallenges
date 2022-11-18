7177418d15e24b7782341a7f1a91503ecf8eed92
Qiese <hoangtrungbi2002@gmail.com>
2022-11-18 15:24:52 +0700

    Edit Tree File

---
+++ Study/CS50/W2/func.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+bool ans_triagle(int a, int b, int c);
+int main(void){
+	int a = 2;
+	int b = 3;
+	int c = 3;
+	bool z = ans_triagle(a,b,c);
+       	printf("Answer %b",z );	
+};
+bool add_two_arg(int a, int b, int c){
+	if a
+};
+	
+	
---
+++ Study/CS50/W2/func.py
@@ -0,0 +1,6 @@
+def ans_triagle(a,b,c):
+    if (a**2 == b**2 + c**2):
+        return True
+    else:
+        return False
+print(ans_triagle(5,3,4))
